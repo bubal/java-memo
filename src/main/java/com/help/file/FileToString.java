f285cfbb98fb4e91f30c0565be5199b7b3dd6a62
bubu <bubu@riseup.net>
2017-03-28 11:29:52 +0200

    Quelques corrections pour améliorer le code

--- src/main/java/com/help/file/FileToString.java
+++ src/main/java/com/help/file/FileToString.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Hashtable;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -17,26 +17,26 @@ public class FileToString {
 		this.filename = filename;
 	}
 
-	public List<String> LineToList() {
+	public List<String> LineToList() throws IOException {
 		
 		List<String> newligneList= new ArrayList<String>();
-		
+		FileReader fr;
 		BufferedReader br;
-		try {
-			br = new BufferedReader(new FileReader(this.filename));
+		
+			fr = new FileReader(this.filename);
+			br = new BufferedReader(fr);
 			String line;
 			while ((line = br.readLine()) != null) {
 				ligneList.add(line);
 			}
 			br.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+			fr.close();
+		
 		this.ligneList = newligneList;
 		return newligneList;
 	}
 	
-	public String [] LineToTab(){
+	public String [] LineToTab() throws IOException{
 		int taille = LineToList().size();
 		String tabString [] = new String[taille];
 		for (int i=0; i < taille; i++){
@@ -45,10 +45,10 @@ public class FileToString {
 		return tabString;
 	}
 
-	public Map<Integer,String> LineToHash(){
+	public Map<Integer,String> LineToHash() throws IOException{
 		
 		int taille = LineToList().size();
-		Map<Integer,String> HashString = new Hashtable<Integer,String>();
+		Map<Integer,String> HashString = new HashMap<Integer,String>();
 		
 		for (int i=0; i < taille; i++){
 			HashString.put(new Integer(i), this.ligneList.get(i));
