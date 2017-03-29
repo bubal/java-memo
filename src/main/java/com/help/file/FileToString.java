61376f263daa98a9e7aac14aee934778605a542d
bubu <bubu@riseup.net>
2017-03-29 11:31:04 +0200

    Ajout de try catch pour la lecture du buffer

--- src/main/java/com/help/file/FileToString.java
+++ src/main/java/com/help/file/FileToString.java
@@ -1,42 +1,56 @@
 package com.help.file;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class FileToString {
-	
+
 	private String filename;
 	private List<String> ligneList;
-	
+
 	public FileToString (String filename){
 		this.filename = filename;
 	}
 
-	public List<String> lineToList() throws IOException {
-		
+	public List<String> lineToList() {
+
 		List<String> newligneList= new ArrayList<>();
-		FileReader fr;
-		BufferedReader br;
-		
-			fr = new FileReader(this.filename);
-			br = new BufferedReader(fr);
+
+		Path path = Paths.get(this.filename);
+		BufferedReader reader = null;
+
+		try {
+			
+			reader = Files.newBufferedReader(path);
 			String line;
-			while ((line = br.readLine()) != null) {
+			while ((line = reader.readLine()) != null) {
 				ligneList.add(line);
 			}
-			br.close();
-			fr.close();
-		
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (reader != null){
+				try {
+					reader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
 		this.ligneList = newligneList;
 		return newligneList;
 	}
-	
-	public String [] lineToTab() throws IOException{
+
+	public String [] lineToTab(){
 		int taille = lineToList().size();
 		String [] tabString = new String[taille];
 		for (int i=0; i < taille; i++){
@@ -45,15 +59,15 @@ public class FileToString {
 		return tabString;
 	}
 
-	public Map<Integer,String> lineToHash() throws IOException{
-		
+	public Map<Integer,String> lineToHash(){
+
 		int taille = lineToList().size();
 		Map<Integer,String> hashString = new HashMap<>();
-		
+
 		for (int i=0; i < taille; i++){
-			hashString.put(new Integer(i), this.ligneList.get(i));
+			hashString.put(Integer.valueOf(i), this.ligneList.get(i));
 		}
 		return hashString;
 	}
-	
+
 }
